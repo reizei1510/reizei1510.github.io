77e6864d586f9fe62510858a3bab51a7289ba958
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-02-03 19:14:25 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,7 +1,7 @@
 function click1(){
     let re = /^\d+$/;
-    let c = document.getElementByName("count");
-    let p = document.getElementByName("price");
+    let c = document.getElementByID("count");
+    let p = document.getElementByID("price");
     let r = document.getElementByID("result");
     if(re.test(f1.value) && re.test(f2.value)){
       r.innerHTML = parseInt(f1.value) * parseInt(f2.value);

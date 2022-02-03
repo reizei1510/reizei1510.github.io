bfe6cb4b53c22d69289040d17d779b20a542ca20
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-02-03 18:49:23 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,19 +1,17 @@
-function click1() {
+function click1(){
     let re = /^\d+$/;
-    let c = document.getElementById("count");
-    let p = document.getElementById("price");
+    let f1 = document.getElementById("field1");
+    let f2 = document.getElementById("field2");
     let r = document.getElementById("result");
-    if (re.test(c[0].value) && re.test(p[0].value)){
-      r.innerHTML = parseInt(c[0].value) * parseInt(p[0].value);
-    }
-    else {
-      r.innerHTML = "Введены некорректные данные";
+    if(re.test(f1.value) && re.test(f2.value)){
+      r.innerHTML = parseInt(f1.value) * parseInt(f2.value);
+    } else{
+      r.innerHTML = "Введены некорректные данные!";
     }
     return false;
   }
 
-window.addEventListener('DOMContentLoaded', function (event) {
+  window.addEventListener('DOMContentLoaded', function (event) {
     console.log("DOM fully loaded and parsed");
-    let b = document.getElementById("button");
-    b.addEventListener("click", click1);
-});
+    document.getElementById("button1").addEventListener("click", click1);
+  });

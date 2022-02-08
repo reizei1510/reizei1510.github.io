a3d4b65941ecf4be45b547f94b3949a98155157f
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-02-08 12:12:22 +0300

    Update script7.js

--- script7.js
+++ script7.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".gallery").slick({
+    $('.slider').slick({
         dots: true,
         infinite: true,
         slidesToShow: 4,
@@ -7,6 +7,13 @@ $(document).ready(function(){
         responsive: [
             {
               breakpoint: 850,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+              }
+            },
+            {
+              breakpoint: 480,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2

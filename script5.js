18e23ac6913fc76c27947493296b8d51471482d1
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-02-12 19:16:08 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -2,11 +2,11 @@
     let t = /^\d+$/;
     let f1 = document.getElementsByName("field1");
     let f2 = document.getElementsByName("field2");
-    let r = document.getElementById("result");
+    let r1 = document.getElementById("result1");
     if(t.test(f1[0].value) && t.test(f2[0].value)){
-      r.innerHTML = parseInt(f1[0].value) * parseInt(f2[0].value);
+      r1.innerHTML = parseInt(f1[0].value) * parseInt(f2[0].value);
     } else {
-      r.innerHTML = "Введены некорректные данные!";
+      r1.innerHTML = "Введены некорректные данные!";
     }
     return false;
   }

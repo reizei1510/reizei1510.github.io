1c36ffd993b3f29c308f6b30c1909c394e27c259
reizei1510 <90310452+reizei1510@users.noreply.github.com>
2022-02-12 21:06:52 +0300

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,12 +1,12 @@
 function click1(){
     let t1 = /^\d+$/;
-    let f1 = document.getElementById("field1");
-    let f2 = document.getElementById("field2");
+    let f1 = document.getElementsByName("field1");
+    let f2 = document.getElementsByName("field2");
     let r1 = document.getElementById("result1");
-    if(t1.test(f1.value) && t1.test(f2.value)){
-      r1.innerHTML = parseInt(f1.value) * parseInt(f2.value);
+    if(t1.test(f1[0].value) && t1.test(f2[0].value)){
+      r1.innerHTML = parseInt(f1[0].value) * parseInt(f2[0].value);
     } else {
-      r1.innerHTML = "Некорректные данные";
+      r1.innerHTML = "Введены некорректные данные!";
     }
     return false;
   }
@@ -17,94 +17,103 @@ function click1(){
   });
 
 let param = {
-    "Options": {
-    "option1": 0,
-    "option2": 6900
+    "options": {
+    "radio1": 0,
+    "radio2": 6900
     },
     "checkbox1": {
-    "prop1": 200,
-    "prop2": 100
+    "ch1": 200,
+    "ch2": 100
     },
-    "prodType":
+    "product":
     [500, 100, 300]
 };
+
 console.log(param);
 let pr=0;
 let pch=0;
 window.addEventListener("DOMContentLoaded", function (event) {
-    let f = document.getElementsById("field3");
-    let r = document.getElementById("result2");
-    let s = document.getElementsByName("prodType");
-    s[0].addEventListener("change", function(event) {
+    let f3 = document.getElementsById("field3");
+    let r2 = document.getElementById("result2");
+    let sel = document.getElementsByName("product");
+    sel[0].addEventListener("change", function(event) {
       let select = event.target;
       console.log(select.value);
-      let re = /^\s*\d+\s*$/;
-      if(re.exec(f[0].value))
+      let t2 = /^\s*\d+\s*$/;
+      if(t2.exec(f3[0].value))
       {
         if (select.value == "1")
-        {r.innerHTML=param.prodType[0]*f[0].value;}
+        {
+            r.innerHTML=param.product[0]*f3[0].value;
+        }
         if (select.value == "2")
         {
-          r.innerHTML=(param.prodType[1]+pr)*f[0].value;
+          r.innerHTML=(param.product[1]+pr)*f3[0].value;
           let rad = document.querySelectorAll(".prodOptions input[type=radio]");
           rad.forEach(function(radio) {
           radio.addEventListener("change", function(event) {
           rad = event.target;
           console.log(rad.value);
-          if(re.exec(f[0].value))
+          if(t2.exec(f3[0].value))
           {
             if(document.getElementById("option1").checked)
             {
-                r.innerHTML=(param.prodType[1]+param.Options.option1)*f[0].value;
+                r2.innerHTML=(param.product[1]+param.Options.option1)*f3[0].value;
                 pr=param.Options.option1;
             }
             if(document.getElementById("option2").checked)
             {
-                r.innerHTML=(param.prodType[1]+param.Options.option2)*f[0].value;
+                r2.innerHTML=(param.product[1]+param.Options.option2)*f3[0].value;
                 pr=param.Options.option2;
             }
             if(document.getElementById("option3").checked)
             {
-                r.innerHTML=(param.prodType[1]+param.Options.option3)*f[0].value;
+                r2.innerHTML=(param.product[1]+param.Options.option3)*f3[0].value;
                 pr=param.Options.option3;
             }
         }
-        else {r.innerHTML = "Введите количество товара";}
+        else {
+            r2.innerHTML = "Введите количество товара";
+        }
         });});
         }
         if (select.value == "3")
         {
-          r.innerHTML=(param.prodType[2]+pch)*f[0].value;
+          r2.innerHTML=(param.product[2]+pch)*f3[0].value;
           let ch = document.querySelectorAll(".checkbox1 input[type=checkbox]");
           ch.forEach(function(checkbox) {
           checkbox.addEventListener("change", function(event) {
           ch = event.target;
           console.log(ch.value);
-          if(re.exec(f[0].value))
+          if(t2.exec(f3[0].value))
           {
             if(document.getElementById("ch1").checked)
             {
-                r.innerHTML=(param.prodType[2]+param.checkbox1.prop1)*f[0].value;
-                pch=param.checkbox1.prop1;
+                r2.innerHTML=(param.product[2]+param.checkbox1.check1)*f3[0].value;
+                pch=param.checkbox1.check1;
             }
             else
             {
-                r.innerHTML=(param.prodType[2])*f[0].value;
+                r2.innerHTML=(param.product[2])*f3[0].value;
                 pch=0;
             }
           }
-          else {r.innerHTML = "Введите количество товара";}
+          else {
+              r2.innerHTML = "Введите количество товара";
+          }
         });
       });
       }
     }
-  else {r.innerHTML = "Введите количество товара";}
+  else {
+      r2.innerHTML = "Введите количество товара";
+  }
     });
-    r.innerHTML="2";
+    r2.innerHTML="2";
     return false;
 });
 window.addEventListener("DOMContentLoaded", function (event) {
-let s = document.getElementsByName("prodType");
+let s = document.getElementsByName("product");
 s[0].addEventListener("change", function(event) {
 let select = event.target;
 let radios = document.getElementById("prodOptions");
@@ -120,7 +129,7 @@ radio.addEventListener("change", function(event) {
 });
 });
 window.addEventListener("DOMContentLoaded", function (event) {
-let s = document.getElementsByName("prodType");
+let s = document.getElementsByName("product");
 s[0].addEventListener("change", function(event) {
   let select = event.target;
   let checkboxes = document.getElementById("checkbox1");
